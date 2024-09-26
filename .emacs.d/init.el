923a5b386086590267ad3a9e5103eec2cbe2aa08
Wilf-bog <vachonfrederic@proton.me>
2024-09-25 22:09:48 -0400

    corfu, yasnippet added, bell redisabled

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -157,7 +157,7 @@
     ;;; (load-theme 'doom-one t)
 					;
     ;;; Enable flashing mode-line on errors
-					;(doom-themes-visual-bell-config)
+					(doom-themes-visual-bell-config)
     ;;; Enable custom neotree theme (all-the-icons must be installed!)
 					;(doom-themes-neotree-config)
     ;;; or for treemacs users
@@ -332,6 +332,30 @@
   (sentence-end-double-space nil)
   (scroll-error-top-bottom t)
   (save-interprogram-paste-before-kill t))
+
+(use-package corfu
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
 
 (use-package flyspell
   :custom
