44b6a68801458d3197cd4be1013a806690cfb23e
Wilf-bog <vachonfrederic@proton.me>
2024-09-28 11:14:25 -0400

    Adding mu4e to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -749,6 +749,43 @@
 
 ;; (setq org-default-notes-file (concat org-directory "/notes.org"))
 
+(use-package mu4e
+  :ensure nil
+  :load-path "/usr/share/emacs/site-lisp/mu4e/"
+  :defer 20 ; Wait until 20 seconds after startup
+  :config
+
+  (setq mu4e-change-filenames-when-moving t ; avoid sync conflicts
+	mu4e-update-interval (* 10 60) ; check mail 10 minutes
+	mu4e-compose-format-flowed t ; re-flow mail so it's not hard wrapped
+	mu4e-get-mail-command "mbsync -a"
+	mu4e-maildir "~/Mail"
+	mu4e-attachment-dir "~/Downloads")
+
+  (setq mu4e-drafts-folder "/Drafts"
+	mu4e-sent-folder   "/Sent"
+	mu4e-refile-folder "/All Mail"
+	mu4e-trash-folder  "/Trash")
+
+  (setq mu4e-maildir-shortcuts
+	'((:maildir "/INBOX"     :key ?i)
+	  (:maildir "/sent"      :key ?s)
+	  (:maildir "/Trash"     :key ?t)
+	  (:maildir "/Drafts"    :key ?d)
+	  (:maildir "/All Mail"  :key ?a)))
+
+  (setq user-mail-address "vachonfrederic@proton.me"
+	user-full-name  "Frédéric Vachon")
+
+  (setq message-send-mail-function 'smtpmail-send-it
+	auth-sources '("~/.authinfo") ;need to use gpg version but only local smtp stored for now
+	smtpmail-smtp-server "127.0.0.1"
+	smtpmail-smtp-service 1025
+	smtpmail-stream-type  'starttls)
+
+  ;; Run mu4e in the background to sync mail periodically
+  (mu4e t))
+
 ;; Denote
 
 (use-package denote
