24246eb506ac4f10b5f825cc48773fd58344465b
Wilf-bog <vachonfrederic@proton.me>
2024-10-31 21:08:17 -0400

    Emacs: Disabling Atkinson

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -188,8 +188,9 @@
 (when (eq system-type 'windows-nt)
   (set-face-attribute 'variable-pitch nil :family "Iosevka Comfy Motion Duo"))
 (when (eq system-type 'gnu/linux)
+  (set-face-attribute 'variable-pitch nil :family "Iosevka Comfy Motion Duo"))
+  ;;(set-face-attribute 'variable-pitch nil :family "Atkinson Hyperlegible"))
   ;; (set-face-attribute 'variable-pitch nil :family "Noto Serif"))
-  (set-face-attribute 'variable-pitch nil :family "Atkinson Hyperlegible"))
 (set-face-attribute 'fixed-pitch nil :family "Iosevka Comfy")
 
 ;; Window management
