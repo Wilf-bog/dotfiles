711acffbfdf52546211608e5f0f800dd7706605a
Frédéric Vachon <vachonfrederic@proton.me>
2024-12-11 22:53:01 -0500

    Emacs: enabling options for corfu

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -312,28 +312,33 @@
   (save-interprogram-paste-before-kill t))
 
 (use-package corfu
-  ;; Optional customizations
-  ;; :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+ :ensure t
+ :bind (:map corfu-map ("<tab>" . corfu-complete))
+ ;; Optional customizations
+ :custom
+ (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+ (corfu-auto t)                 ;; Enable auto completion
+ ;; (corfu-separator ?\s)          ;; Orderless field separator
+ ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+ ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+ ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+ ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+ ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+ ;; (corfu-scroll-margin 5)        ;; Use scroll margin
 
-  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
+ ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+ ;; :hook ((prog-mode . corfu-mode)
+ ;;        (shell-mode . corfu-mode)
+ ;;        (eshell-mode . corfu-mode))
 
-  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
-  ;; be used globally (M-/).  See also the customization variable
-  ;; `global-corfu-modes' to exclude certain modes.
-  :init
-  (global-corfu-mode))
+ ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+ ;; be used globally (M-/).  See also the customization variable
+ ;; `global-corfu-modes' to exclude certain modes.
+ :init
+ (global-corfu-mode)
+ (with-eval-after-load 'savehist
+   (corfu-history-mode 1)
+   (add-to-list 'savehist-additional-variables 'corfu-history)))
 
 (use-package flyspell
   :custom
