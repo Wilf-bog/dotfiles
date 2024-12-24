3f58fe0f51bcefc1c2368482f668551a1035b71f
Frédéric Vachon <vachonfrederic@proton.me>
2024-12-24 11:57:56 -0500

    Emacs: adding scrolling followup function

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -340,6 +340,31 @@
    (corfu-history-mode 1)
    (add-to-list 'savehist-additional-variables 'corfu-history)))
 
+(use-package emacs
+  :config
+  (setq-default scroll-preserve-screen-position t)
+  (setq-default scroll-conservatively 1) ; affects `scroll-step'
+  (setq-default scroll-margin 0)
+
+  (define-minor-mode prot/scroll-centre-cursor-mode
+    "Toggle centred cursor scrolling behaviour."
+    :init-value nil
+    :lighter " S="
+    :global nil
+    (if prot/scroll-centre-cursor-mode
+        (setq-local scroll-margin (* (frame-height) 2)
+                    scroll-conservatively 0
+                    maximum-scroll-margin 0.5)
+      (dolist (local '(scroll-preserve-screen-position
+                       scroll-conservatively
+                       maximum-scroll-margin
+                       scroll-margin))
+        (kill-local-variable `,local))))
+
+  ;; C-c l is used for `org-store-link'.  The mnemonic for this is to
+  ;; focus the Line and also works as a variant of C-l.
+  :bind ("C-c L" . prot/scroll-centre-cursor-mode))
+
 (use-package flyspell
   :custom
   (ispell-program-name "hunspell")
