13d25c47266036e2ced4ccfac11186b2ee01a30f
Frédéric Vachon <vachonfrederic@proton.me>
2025-01-25 20:00:21 -0500

    Emacs: changing picom blur

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -697,6 +697,28 @@
 ;;   (prot-elfeed-fontify-tags))
 ;;
 
+(use-package elfeed-tube
+  :ensure t ;; or :straight t
+  :after elfeed
+  :demand t
+  :config
+  ;; (setq elfeed-tube-auto-save-p nil) ; default value
+  ;; (setq elfeed-tube-auto-fetch-p t)  ; default value
+  (elfeed-tube-setup)
+
+  :bind (:map elfeed-show-mode-map
+         ("F" . elfeed-tube-fetch)
+         ([remap save-buffer] . elfeed-tube-save)
+         :map elfeed-search-mode-map
+         ("F" . elfeed-tube-fetch)
+         ([remap save-buffer] . elfeed-tube-save)))
+
+(use-package elfeed-tube-mpv
+  :ensure t ;; or :straight t
+  :bind (:map elfeed-show-mode-map
+              ("C-c C-f" . elfeed-tube-mpv-follow-mode)
+              ("C-c C-w" . elfeed-tube-mpv-where)))
+
 ;; Easy insertion of weblinks
 
 (use-package org-web-tools
