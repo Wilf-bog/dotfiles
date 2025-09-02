a2f2af192960680ab0821f7ea08c21174bf1da55
Frédéric Vachon <vachonfrederic@proton.me>
2025-09-01 22:19:37 -0400

    Emacs: starting a new config (and keeping the older one)

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,8 @@
+(setq frame-resize-pixelwise t
+      frame-title-format '("%b")	; Name of file as the title of the frame
+      use-file-dialog nil
+      use-short-answers t
+      inhibit-startup-screen t
+      inhibit-x-resources t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-startup-buffer-menu t)
