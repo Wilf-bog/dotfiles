f1c0102c0c8c5b5ac0a84f09bc81ef3b02b6c756
Frédéric Vachon <vachonfrederic@proton.me>
2025-09-02 20:26:08 -0400

    Emacs: fixing typo and error in block sources

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,8 +2,36 @@
 
 
 
+;; Set package archives
+
+(use-package package
+  :config
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/"))
+  (package-initialize))
+
+;; Package Management
+
+(use-package use-package
+  :custom
+  (use-package-always-ensure nil)	; older config had it true.
+  (package-native-compile t)
+  (warning-minimum-level :emergency))
+
+(setq custom-safe-themes t)
+
+(load-theme 'modus-vivendi-tinted :no-confirm)
+
 (use-package magit
   :ensure t)
 
 (add-hook 'magit-process-find-password-functions
 	  'magit-process-password-auth-source)
+
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides
+   '((file (styles partial-completion)))))
