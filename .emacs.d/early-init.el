ce576488056336fe764c7fa056a766c1da0a6d81
Frédéric Vachon <vachonfrederic@proton.me>
2025-09-02 23:03:52 -0400

    Emacs: big update with early init and vertico

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -15,3 +15,23 @@
 
 (setq gc-cons-threshold (* 10 128 1024 1024))
 (setq garbage-collection-messages nil)
+
+(require 'package)
+
+;;; Setup Emacs Lisp Package Archives (ELPAs)
+;; where to get packages to install
+(when (version< emacs-version "28")
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+(add-to-list 'package-archives '("stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+;;; Configure ELPA priorities
+;; Prefer GNU sources and stable versions before development versions from MELPA.
+(customize-set-variable 'package-archive-priorities
+                        '(("gnu"    . 99)   ; prefer GNU packages
+                          ("nongnu" . 80)   ; use non-gnu packages if
+                                        ; not found in GNU elpa
+                          ("stable" . 70)   ; prefer "released" versions
+                                        ; from melpa
+                          ("melpa"  . 0)))  ; if all else fails, get it
+                                        ; from melpa
